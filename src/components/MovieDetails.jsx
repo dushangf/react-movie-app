fa734f3b68112869a4dfb78c966d3abce6b74c2d
dushangf <dushangf@gmail.com>
2022-03-05 00:55:04 +0530

    minor bugs

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaTimes, FaStar } from 'react-icons/fa';
 
 const MovieDetails = ({ setmodalIsOpen, selected, imgURL, selectedGenres }) => {
